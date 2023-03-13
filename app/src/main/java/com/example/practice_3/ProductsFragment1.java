7184660cb42a91db946e5c17d23ea22a33623116
dyxovka <varia.mazurovaaa@gmail.com>
2023-03-13 19:48:23 +0300

    добавление listview и адаптера к нему

--- app/src/main/java/com/example/practice_3/ProductsFragment1.java
+++ app/src/main/java/com/example/practice_3/ProductsFragment1.java
@@ -12,6 +12,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,34 +21,24 @@ import com.example.practice_3.databinding.FragmentProducts1Binding;
 
 public class ProductsFragment1 extends Fragment {
     private static String TAG = "tag";
+    ListView listView;
     FragmentProducts1Binding fragmentProducts1Binding;
-
-
     public ProductsFragment1() {
         // Required empty public constructor
     }
-
     public static ProductsFragment1 newInstance(String param1, String param2) {
         ProductsFragment1 fragment = new ProductsFragment1();
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast toast = Toast.makeText(getContext(), "onCreate", Toast.LENGTH_LONG);
-        toast.show();
-        Log.d(TAG, "onCreate");
     }
-
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
-        Log.d(TAG, "onAttach");
-        Toast toast = Toast.makeText(getContext(), "onAttach", Toast.LENGTH_LONG);
-        toast.show();
     }
 
     private void buttonsBinding(Bundle bundle) {
@@ -54,46 +46,32 @@ public class ProductsFragment1 extends Fragment {
             @Override
             public void onClick(View view) {
                 LoginFragment loginFragment = new LoginFragment();
-                Bundle bundle = getArguments();
-                String name = "";
-                String phone = "";
-                if (bundle != null) {
-                    name = bundle.getString("name");
-                    phone = bundle.getString("phone");
-                }
 
-                Bundle bundle1 = new Bundle();
-                bundle1.putString("name", name);
-                bundle1.putString("phone", phone);
-                loginFragment.setArguments(bundle1);
                 FragmentTransaction fragm = getFragmentManager().beginTransaction();
                 fragm.replace(R.id.fragment_container, loginFragment).commit();
             }
         });
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         fragmentProducts1Binding = fragmentProducts1Binding.inflate(inflater, container, false);
         buttonsBinding(new Bundle());
-        Log.d(TAG, "onCreateView");
-        Toast toast = Toast.makeText(getContext(), "onCreateView", Toast.LENGTH_LONG);
-        toast.show();
         return fragmentProducts1Binding.getRoot();
     }
-
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Log.d(TAG, "onViewCreated");
-        Toast toast = Toast.makeText(getContext(), "onViewCreated", Toast.LENGTH_LONG);
-        toast.show();
-        Bundle bundle = getArguments();
-        if (bundle != null) {
-            String recieveInfo = bundle.getString("name");
-            TextView editText = view.findViewById(R.id.textView5_for_name);
-            editText.setText(recieveInfo);
+
+        listView = fragmentProducts1Binding.listView;
+        String[]  names = new String[200];
+        for(int i = 0; i < 200; i++){
+            String byket = "Букет " + i;
+            names[i] = byket;
+
         }
+        Adapter_ListView adapter_listView = new Adapter_ListView(getContext(), names);
+        listView.setAdapter(adapter_listView);
     }
 }
--- app/src/main/java/com/example/practice_3/ProductsFragment2.java
+++ app/src/main/java/com/example/practice_3/ProductsFragment2.java
@@ -1,47 +1,34 @@
 package com.example.practice_3;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProductsFragment2#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.practice_3.databinding.FragmentProducts1Binding;
+
 public class ProductsFragment2 extends Fragment {
+    private static String TAG = "tag";
+    FragmentProducts1Binding fragmentProducts1Binding;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public ProductsFragment2() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProductsFragment2.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ProductsFragment2 newInstance(String param1, String param2) {
         ProductsFragment2 fragment = new ProductsFragment2();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +36,64 @@ public class ProductsFragment2 extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        Toast toast = Toast.makeText(getContext(), "onCreate", Toast.LENGTH_LONG);
+        toast.show();
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Log.d(TAG, "onAttach");
+        Toast toast = Toast.makeText(getContext(), "onAttach", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    private void buttonsBinding(Bundle bundle) {
+        fragmentProducts1Binding.buttonGotovo1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginFragment loginFragment = new LoginFragment();
+                Bundle bundle = getArguments();
+                String name = "";
+                String phone = "";
+                if (bundle != null) {
+                    name = bundle.getString("name");
+                    phone = bundle.getString("phone");
+                }
+
+                Bundle bundle1 = new Bundle();
+                bundle1.putString("name", name);
+                bundle1.putString("phone", phone);
+                loginFragment.setArguments(bundle1);
+                FragmentTransaction fragm = getFragmentManager().beginTransaction();
+                fragm.replace(R.id.fragment_container, loginFragment).commit();
+            }
+        });
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_products2, container, false);
+        fragmentProducts1Binding = fragmentProducts1Binding.inflate(inflater, container, false);
+        buttonsBinding(new Bundle());
+        Log.d(TAG, "onCreateView");
+        Toast toast = Toast.makeText(getContext(), "onCreateView", Toast.LENGTH_LONG);
+        toast.show();
+        return fragmentProducts1Binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.d(TAG, "onViewCreated");
+        Toast toast = Toast.makeText(getContext(), "onViewCreated", Toast.LENGTH_LONG);
+        toast.show();
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            String recieveInfo = bundle.getString("name");
+            TextView editText = view.findViewById(R.id.textView5_for_name);
+            editText.setText(recieveInfo);
+        }
     }
 }
